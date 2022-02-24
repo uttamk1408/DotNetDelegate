4fe9ca35c18c25574291813e957056dd9ca691db
uttamk1408 <uttamkumar@Uttams-MacBook-Pro.local>
2022-02-24 23:21:02 +0530

    delegate callback example

--- delegate/Program.cs
+++ delegate/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+// public class Program
+// {
+    // public static void Main(string[] args)
+    // {
+        LongRunningClass objC = new LongRunningClass();
+        objC.LongRunningFunction(callbackfunc);
+    // }
+
+    void callbackfunc(int i)
+    {
+        Console.WriteLine(i.ToString());
+    }
+// }
+
+public class LongRunningClass
+{
+    public delegate void callbackfunc(int i);
+    public void LongRunningFunction(callbackfunc obj)
+    {
+        for(int i = 0; i <= 1000; i++)
+        {
+            obj(i);
+        }
+    }
+}
